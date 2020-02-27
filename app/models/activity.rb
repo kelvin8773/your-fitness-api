13641cc6be3a8d5b535fe75a375052024a4417e3
Kelvin Liang <kelvin9877@gmail.com>
2020-02-28 01:10:05 +0800

    update user & activity model file to pass the test

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,5 @@
 class Activity < ApplicationRecord
   belongs_to :user
+
+  validates_presence_of :type, :amount
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :activities, dependent: :destroy
+
+  #validations
+  validates_presence_of :name, :age, :sex
 end
