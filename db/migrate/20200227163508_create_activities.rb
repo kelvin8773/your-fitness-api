3278e2b1fc914218392207a52a8cb7fe62cb07f3
Kelvin Liang <kelvin9877@gmail.com>
2020-02-28 00:43:09 +0800

    update db field define error

--- db/migrate/20200227163508_create_activities.rb
+++ db/migrate/20200227163508_create_activities.rb
@@ -2,7 +2,7 @@ class CreateActivities < ActiveRecord::Migration[6.0]
   def change
     create_table :activities do |t|
       t.string :type
-      t.number :amount
+      t.integer :amount
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
