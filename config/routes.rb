872d48c1314c947bb08a3d1be75703c61b03c1d3
Kelvin Liang <kelvin9877@gmail.com>
2020-02-28 12:20:41 +0800

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :users do
+    resources :activities
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
