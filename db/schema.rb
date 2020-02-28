efad5bcb9e43fe1a542f07e74e65e880518cdc2e
Kelvin Liang <kelvin9877@gmail.com>
2020-02-28 15:59:06 +0800

    linter fix again

--- db/schema.rb
+++ db/schema.rb
@@ -12,22 +12,22 @@
 
 ActiveRecord::Schema.define(version: 2020_02_27_163508) do
 
-  create_table "activities", force: :cascade do |t|
-    t.string "type"
-    t.integer "amount"
-    t.integer "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_activities_on_user_id"
+  create_table 'activities', force: :cascade do |t|
+    t.string 'type'
+    t.integer 'amount'
+    t.integer 'user_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['user_id'], name: 'index_activities_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "sex"
-    t.integer "age"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'sex'
+    t.integer 'age'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  add_foreign_key "activities", "users"
+  add_foreign_key 'activities', 'users'
 end
