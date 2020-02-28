2600cb90198db2839abcd781eda1c0a472f03064
Kelvin Liang <kelvin9877@gmail.com>
2020-02-28 16:01:12 +0800

    linter fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_02_27_163508) do
-
   create_table 'activities', force: :cascade do |t|
     t.string 'type'
     t.integer 'amount'
