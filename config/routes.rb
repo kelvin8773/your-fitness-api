778228bb838add36af4bd9ca2405b9bfa7a5d209
Kelvin Liang <kelvin9877@gmail.com>
2020-03-01 19:58:50 +0800

    updating route to adapt local development

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :users do
-    resources :activities
-  end
-
+  scope :api do
+    resources :users do
+      resources :activities
+    end
+  end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
