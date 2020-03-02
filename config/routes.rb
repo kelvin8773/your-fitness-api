5a68ad3016521c0cc08bcd581eb60ef701bf7cfd
Kelvin Liang <kelvin9877@gmail.com>
2020-03-02 19:28:16 +0800

    reverse back to original URI

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  scope :api do
+  # scope :api do
     resources :users do
       resources :activities
     end
-  end 
+  # end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
